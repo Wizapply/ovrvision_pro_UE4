e8485399a19d6218c9c950f5a89e16aa2a21dc96
WizapplyTaniguchi <y.taniguchi@wizapply.com>
2017-03-10 19:01:11 +0900

    Fix file path

--- Source/OvrvisionUE/OvrvisionBlueprintLibrary.cpp
+++ Source/OvrvisionUE/OvrvisionBlueprintLibrary.cpp
@@ -5,33 +5,6 @@
 
 #include "IOvrvisionPlugin.h"
 
-//UOvrvisionBlueprintLibrary
-/*
-UOvrvisionBlueprintLibrary::UOvrvisionBlueprintLibrary()
-{
-	OvrvisionLeftTexture = NULL;
-	OvrvisionRightTexture = NULL;
-
-	GetImageWidth = 0;
-	GetImageHeight = 0;
-	GetImageAspectH = 0.0f;
-
-	GetFocalPoint = 0.0f;
-	GetHMDRightGap.Set(0.0f, 0.0f, 0.0f);
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ovrvision")
-		int32 GetImageWidth;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ovrvision")
-		int32 GetImageHeight;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ovrvision")
-		float GetImageAspectH;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ovrvision")
-		float GetFocalPoint;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ovrvision")
-		FVector GetHMDRightGap;
-
-}
-*/
 namespace {
 
 	UTexture2D* OvrvisionLeftTexture;
@@ -43,17 +16,6 @@ void UOvrvisionBlueprintLibrary::OpenOvrvision(int32 openType, float ar_size)
 	IOvrvisionPlugin::Get().Open(openType, 0.0f);
 	OvrvisionLeftTexture = IOvrvisionPlugin::Get().GetTextureLeft();
 	OvrvisionRightTexture = IOvrvisionPlugin::Get().GetTextureRight();
-
-	/*
-	GetImageWidth = IOvrvisionPlugin::Get().GetImageWidth();
-	GetImageHeight = IOvrvisionPlugin::Get().GetImageHeight();
-	GetImageAspectH = (float)GetImageHeight / (float)GetImageWidth;
-
-	GetFocalPoint = IOvrvisionPlugin::Get().GetFocalPoint();
-	GetHMDRightGap.X = IOvrvisionPlugin::Get().GetHMDRightGap(0);
-	GetHMDRightGap.Y = IOvrvisionPlugin::Get().GetHMDRightGap(1);
-	GetHMDRightGap.Z = IOvrvisionPlugin::Get().GetHMDRightGap(2);
-	*/
 }
 
 void UOvrvisionBlueprintLibrary::CloseOvrvision()
